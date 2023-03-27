c2327a2fb42b71d8e604ae712ae3598ed1dc9702
Abody462 <ajisuwandikanugraha@gmail.com>
2023-03-27 15:06:08 +0700

    menambah 4 file baru

---
+++ view-latihan.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>latihan1</title>
+</head>
+<body>
+Halo Kawan.. Yuk kita belajar web programming..!!!<br> 
+Nilai 1 = <?= $nilai1; ?> <br>
+Nilai 2 = <?= $nilai2; ?> <br>
+ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
